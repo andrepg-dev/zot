06e8dcc06abfdb8c0f93fb674eeeef1c4152e0e4
André Ponce <asponceg@gmail.com>
2026-01-07 16:20:35 -0600

    Ajusting files

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,17 +1,17 @@
-import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 type Env = {
   DATABASE_URL: string;
 };
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: "prisma/schema.prisma",
   migrations: {
-    path: 'prisma/migrations',
+    path: "prisma/migrations",
   },
-  engine: 'classic',
+  engine: "classic",
   datasource: {
-    url: env<Env>('DATABASE_URL'),
+    url: env<Env>("DATABASE_URL"),
   },
 });
