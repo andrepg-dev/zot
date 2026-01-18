1c85a8396ef6df26c94e21c445ee8187280571cd
André Ponce <asponceg@gmail.com>
2026-01-17 22:46:12 -0600

    Adating user schema for differents provider

--- apps/api/src/users/schemas/users.schema.ts
+++ apps/api/src/users/schemas/users.schema.ts
@@ -1,20 +1,30 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
-@Schema({
-  timestamps: true,
-})
+@Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true, maxLength: 100, min: 2 })
-  name: string;
+  @Prop({ required: false, maxLength: 100, min: 2 })
+  name?: string;
 
-  @Prop({ required: true, maxLength: 100, min: 2 })
-  last_name: string;
+  @Prop({ required: false, maxLength: 100, min: 2 })
+  last_name?: string;
 
-  @Prop({ required: true, unique: true, maxLength: 100 })
+  @Prop({ required: true, unique: true, maxLength: 100, index: true })
   email: string;
 
   @Prop({ required: true })
   username: string;
+
+  @Prop({ required: false })
+  password?: string;
+
+  @Prop({ default: "local" }) // Local, Google, Github
+  provider: string;
+
+  @Prop()
+  providerId?: string;
+
+  @Prop()
+  avatar?: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
