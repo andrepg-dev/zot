00a2e3a051f8365c6025fdeae4f6517077ed4fc5
André Ponce <asponceg@gmail.com>
2026-01-20 22:51:07 -0600

    Refactor JwtAuthGuard to include public route handling with Reflector

--- apps/api/src/auth/guards/jwt.guard.ts
+++ apps/api/src/auth/guards/jwt.guard.ts
@@ -1,16 +1,29 @@
 import { ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
 import { Observable } from "rxjs";
+import { IS_PUBLIC_KEY } from "../decorators/skip-auth.decorator";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
-  constructor() {
+  constructor(private reflector: Reflector) {
     super();
   }
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    // reflector lee la metadata de la siguiente manera:
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      // Only this is needeed
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     return super.canActivate(context);
   }
 }
