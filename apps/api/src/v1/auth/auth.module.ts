51233b2d7cf0294a26e265aa69b5e543f320d080
André Ponce <asponceg@gmail.com>
2026-01-31 11:00:15 -0600

    fix: UserModule import

--- apps/api/src/v1/auth/auth.module.ts
+++ apps/api/src/v1/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 
 import { CookiesService } from "@api/src/common/cookies.service";
 import { JwtServicesModule } from "@api/src/common/jwt-services/jwt-services.module";
+import { UsersModule } from "../users/users.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { GitHubStrategy } from "./strategies/github.strategy";
@@ -10,7 +11,7 @@ import { JwtStrategy } from "./strategies/jwt.strategy";
 import { LocalStrategy } from "./strategies/local.strategy";
 
 @Module({
-  imports: [UsersModuleV1, JwtServicesModule],
+  imports: [UsersModule, JwtServicesModule],
   controllers: [AuthController],
   providers: [
     CookiesService,
