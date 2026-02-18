d2904d89f2ad70e6d1f88b64ea4ba700c268ee7b
André Ponce <asponceg@gmail.com>
2026-02-18 09:06:05 -0600

    Adding logo loop

--- apps/landing-page/app/page.tsx
+++ apps/landing-page/app/page.tsx
@@ -1,7 +1,19 @@
 import DarkVeil from "@/components/DarkVeil";
+import LogoLoop from "@/components/LogoLoop";
 import ShinyText from "@/components/ShinyText";
+import Link from "next/link";
 
 export default function HomePage() {
+  const techLogos = [
+    {
+      src: "/icons/zot-icon-only.svg",
+      alt: "Zot",
+      title: "Zot",
+      width: 64,
+      height: 64
+    },
+  ];
+
   return (
     <div className="font-sans">
 
@@ -16,38 +28,70 @@ export default function HomePage() {
         />
       </div>
 
-      <div aria-label="Hero section" className="flex justify-center items-center w-full h-screen flex-col gap-6">
-        <div className="bg-black px-6 pr-2 py-2 rounded-full border flex items-center gap-4">
-          <ShinyText
-            text="Start measuring your clients"
-            speed={2}
-            delay={0}
-            color="#b5b5b5"
-            shineColor="#ffffff"
-            spread={120}
-            direction="left"
-            yoyo={false}
-            pauseOnHover={false}
-            disabled={false}
-          />
+      <header className="px-12 py-9 text-muted-foreground absolute">
+        <div className="flex gap-4 items-center">
+          <Link href={"#integration"}>Integration</Link>
+          <Link href={"#integration"}>Pricing</Link>
+          <Link href={"#integration"}>Testimonial</Link>
+          <Link href={"#integration"}>Contact</Link>
+        </div>
+      </header>
 
-          <button className="bg-zinc-900 rounded-full text-white px-4 py-1 text-sm">
-            Join now
-          </button>
+      <div aria-label="Hero section" className="flex justify-center items-center w-full h-screen flex-col">
+        <div className="mb-26 flex justify-center items-center w-full gap-6 flex-col">
+          <div className="bg-black/30 hover:bg-zinc-600/10 transition backdrop-blur-md px-6 pr-2 py-2 rounded-full border flex items-center gap-4 cursor-pointer">
+            <ShinyText
+              text="Start measuring your clients"
+              speed={2}
+              delay={0}
+              color="#b5b5b5"
+              shineColor="#ffffff"
+              spread={120}
+              direction="left"
+              yoyo={false}
+              pauseOnHover={false}
+              disabled={false}
+            />
+
+            <button className="bg-zinc-900 rounded-full text-white px-4 py-1 text-sm">
+              Join now
+            </button>
+          </div>
+
+          <h1 className="text-6xl font-light">Build, measure, feedback</h1>
+          <h3 className="text-muted-foreground italic text-xl text-center w-[50ch]">
+            For developers launching new products, analytics and tools should be the backbone. We provide it.
+          </h3>
+
+          <div className="flex gap-6">
+            <button className="bg-zinc-900 text-foreground px-6 py-1.5 border rounded-full cursor-pointer">Get started</button>
+            <button className="backdrop-blur-md px-6 py-1.5 rounded-full cursor-pointer">Start free trial</button>
+          </div>
         </div>
 
-        <h1 className="text-5xl">Build, measure, feedback</h1>
-        <h3 className="text-muted-foreground italic text-xl text-center">
-          For developers launching new products, analytics and tools should be the backbone. <br /> We provide it.
-        </h3>
+        <div className="absolute bottom-16 left-1/2 -translate-x-1/2 flex flex-col items-center gap-3">
+          <span className="text-sm text-foreground/80 text-center">
+            Join over 1,000 startups and start to maximize your productivity.
+          </span>
+
+          <div className="h-[100px] w-[75%] relative overflow-hidden">
+            <LogoLoop
+              logos={techLogos}
+              speed={30}
+              direction="left"
+              logoHeight={60}
+              gap={60}
+              hoverSpeed={20}
+              scaleOnHover
+              fadeOut
+              fadeOutColor="#000"
+              ariaLabel="Technology partners"
+            />
+          </div>
+        </div>
+
+
       </div>
-
-
-
-
-
-
-
     </div >
   )
 }
