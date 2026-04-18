a988a000ab8ffbff2c322c90417b5812ccc6cbf7
André Ponce <asponceg@gmail.com>
2026-04-18 17:12:01 -0600

    remove design system directory and remove commented lines from docker compose

--- packages/design-system/.storybook/provider.tsx
+++
@@ -1,6 +0,0 @@
-import { HeroUIProvider } from "@heroui/system";
-import React from "react";
-
-export default function StoryProvider({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
-}
--- packages/design-system/src/components/Type/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Type } from "./Type";
-export type { TypeProps } from "./Type";
